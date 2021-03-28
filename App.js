5fb0c4f963b6bb768db7a1ffd53f3d230a40e41d
vigyal <31916211+vigyal@users.noreply.github.com>
2021-03-28 12:16:29 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,28 +1,28 @@
-import React from 'react';
+import React from "react";
 
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer } from "@react-navigation/native";
 
-import { Restaurant, OrderDelivery } from './screens'
-import Tabs from './navigation/tabs'
+import { Restaurant, OrderDelivery } from "./screens";
+import Tabs from "./navigation/tabs";
 
 const Stack = createStackNavigator();
 
 const App = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{
-                    headerShown: false
-                }}
-                initialRouteName={'Home'}
-            >
-                <Stack.Screen name="Home" component={Tabs} />
-                <Stack.Screen name="Restaurant" component={Restaurant} />
-                <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+        initialRouteName={"Home"}
+      >
+        <Stack.Screen name="Home" component={Tabs} />
+        <Stack.Screen name="Restaurant" component={Restaurant} />
+        <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
--- constants/maps.js
+++ constants/maps.js
@@ -1,3 +1,3 @@
-const GOOGLE_API_KEY = "AIzaSyD_lMXvGFGsiO2qhvX7Vs49r-sdICRtrAQ"
+const GOOGLE_API_KEY = "AIzaSyBO-XcjAUFys7anXmutD2qTr6c8q4ZaN2Q";
 
-export default GOOGLE_API_KEY
+export default GOOGLE_API_KEY;
